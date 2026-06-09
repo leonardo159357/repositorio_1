eb67870177c7555c3b20fc433f3b567d316405cc
leonardo159357 <gato.chess123@gmail.com>
2026-06-09 17:59:45 -0500

    Palabras que no se entienden:LOL,CRINGE,etc

--- main.py
+++ main.py
@@ -1,1 +1,38 @@
-print("Hello world!!")
+meme_dict = {
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Una respuesta común a algo gracioso", 
+            "ROFL" : "Una respuesta a una broma", 
+            "SHEESH": "Ligera desaprobación",
+            "AGGRO" :"Ponerse agresivo o enojado" 
+}
+print("Estás son las instrucciones:Escribe bien tus palabras y en mayúsculas, y estas son las palabras: CRINGE,LOL,ROFL,SHEESH,AGGRO, por si las dudas   ")
+palabra = input("Escribe una palabraque no entiendas(Con mayúsculas): ")
+
+if palabra in meme_dict.keys():
+    print(meme_dict[palabra])
+else:
+    print("Palabra no encontrada")
+palabra = input("Escribe una palabraque no entiendas(Con mayúsculas): ")
+
+if palabra in meme_dict.keys():
+    print(meme_dict[palabra])
+else:
+    print("Palabra no encontrada")
+palabra = input("Escribe una palabraque no entiendas(Con mayúsculas): ")
+
+if palabra in meme_dict.keys():
+    print(meme_dict[palabra])
+else:
+    print("Palabra no encontrada")
+palabra = input("Escribe una palabraque no entiendas(Con mayúsculas): ") 
+
+if palabra in meme_dict.keys():
+    print(meme_dict[palabra])
+else:
+    print("Palabra no encontrada")
+palabra = input("Escribe una palabraque no entiendas(Con mayúsculas): ") 
+
+if palabra in meme_dict.keys():
+    print(meme_dict[palabra])
+else:
+    print("Palabra no encontrada")
